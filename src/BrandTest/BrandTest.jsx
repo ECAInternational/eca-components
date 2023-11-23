858ad5eaef7cdc5f1f6573d6c7102030f100b008
Paul Graves <paul.graves@eca-international.com>
2023-11-23 16:32:22 +0000

    Feat: Change Brand Button Text

--- src/BrandTest/BrandTest.jsx
+++ src/BrandTest/BrandTest.jsx
@@ -4,7 +4,7 @@ export const BrandTest = () => {
   return (
     <>
       <button className='bg-red-600'>Red 600</button>
-      <button className='bg-red-200'>Red 200</button>
+      <button className='bg-red-200'>Red 200 Test</button>
     </>
   );
 };
