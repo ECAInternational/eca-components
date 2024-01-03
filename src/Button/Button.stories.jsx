5a0c94e46929eeb1570ecc7fd9f80cdf58d9739a
Paul Graves <paul.graves@eca-international.com>
2024-01-03 16:26:34 +0000

    Try to get interaction to work

--- src/Button/Button.stories.jsx
+++ src/Button/Button.stories.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { userEvent, waitFor, within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
+import { userEvent, within } from '@storybook/testing-library';
 import { Button } from './Button';
 
 export default {
@@ -156,17 +155,15 @@ export const Interactions = {
     variant: 'primary',
     children: 'Interactions'
   },
-  play: async ({ args, canvasElement, step }) => {
+  play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement);
 
     await step('Click', async () => {
       await userEvent.click(canvas.getByRole('button'));
-      await waitFor(() => expect(args.onClick).toHaveBeenCalled());
     });
 
     await step('Hover', async () => {
       await userEvent.hover(canvas.getByRole('button'));
-      await waitFor(() => expect(args.onMouseOver).toHaveBeenCalled());
     });
   }
 };
