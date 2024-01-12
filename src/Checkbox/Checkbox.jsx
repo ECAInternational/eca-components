4b3eaf307013a66f80d78525961fbe658c46a1f7
Paul Graves <paul.graves@eca-international.com>
2024-01-12 14:26:14 +0000

    Feat: More checkbox work

--- src/Checkbox/Checkbox.jsx
+++ src/Checkbox/Checkbox.jsx
@@ -5,29 +5,31 @@ export function Checkbox(props) {
   const { leftLabel, rightLabel, id, disabled, ...others } = props;
 
   return (
-    <div className='flex items-center'>
+    <div>
       {leftLabel && (
         <label htmlFor={id} className={`text-sm font-light se-2 ${disabled ? 'text-controls-content-disabled' : 'text-neutral-body'}`}>
           {leftLabel}
         </label>
       )}
 
-      <input
-        type='checkbox'
-        id={id}
-        disabled={disabled}
-        {...others}
-        className='appearance-none w-6 h-6 border rounded unchecked:border-controls-border
-      checked:border-controls-highlight checked:bg-controls-highlight
-      hover:ring-2 hover:ring-offset-1
-      focus-visible:ring-2 focus-visible:ring-offset-1
-      checked:hover:ring-controls-highlight-paler
-      unchecked:hover:ring-controls-border-hover
-      checked:focus-visible:ring-controls-highlight
-      unchecked:focus-visible:border-controls-border
-      active:border-2 active:border-controls-highlight-palest'
-      />
-
+      <div>
+        <input
+          type='checkbox'
+          id={id}
+          disabled={disabled}
+          {...others}
+          className='peer shrink-0 mt-1 relative appearance-none w-6 h-6 border rounded unchecked:border-controls-border
+        checked:border-controls-highlight checked:bg-controls-highlight
+        hover:ring-2 hover:ring-offset-1
+        focus-visible:ring-2 focus-visible:ring-offset-1
+        checked:hover:ring-controls-highlight-paler
+        unchecked:hover:ring-controls-border-hover
+        checked:focus-visible:ring-controls-highlight
+        unchecked:focus-visible:border-controls-border
+        active:border-2 active:border-controls-highlight-palest'
+        />
+        <i className='fi fi-br-check w-6 h-6 mt-1 text-controls-highlight-palest absolute hidden peer-checked:block' />
+      </div>
       {rightLabel && (
         <label htmlFor={id} className={`text-sm font-light ps-2  ${disabled ? 'text-controls-content-disabled' : 'text-neutral-body'}`}>
           {rightLabel}
