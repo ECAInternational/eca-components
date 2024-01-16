b1990f82d26b113f8a3e3892f148372153738e10
Paul Graves <paul.graves@eca-international.com>
2024-01-16 16:36:25 +0000

    Feat: More changes

--- src/Checkbox/Checkbox.jsx
+++ src/Checkbox/Checkbox.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export function Checkbox(props) {
-  const { leftLabel, rightLabel, id, disabled, ...others } = props;
+  const { leftLabel, rightLabel, id, checked, disabled, ...others } = props;
 
   return (
-    <div>
+    <div className='flex items-center'>
       {leftLabel && (
-        <label htmlFor={id} className={`text-sm font-light se-2 ${disabled ? 'text-controls-content-disabled' : 'text-neutral-body'}`}>
+        <label htmlFor={id} className={`text-sm font-light pe-2 ${disabled ? 'text-controls-content-disabled' : 'text-neutral-body'}`}>
           {leftLabel}
         </label>
       )}
 
-      <div>
+      <div className='flex items-center'>
         <input
           type='checkbox'
           id={id}
           disabled={disabled}
+          checked={checked}
           {...others}
-          className='peer shrink-0 mt-1 relative appearance-none w-6 h-6 border rounded unchecked:border-controls-border
+          className='peer cursor-pointer appearance-none w-6 h-6 border rounded transition-all
+          unchecked:border-controls-border
         checked:border-controls-highlight checked:bg-controls-highlight
         hover:ring-2 hover:ring-offset-1
         focus-visible:ring-2 focus-visible:ring-offset-1
@@ -28,7 +30,10 @@ export function Checkbox(props) {
         unchecked:focus-visible:border-controls-border
         active:border-2 active:border-controls-highlight-palest'
         />
-        <i className='fi fi-br-check w-6 h-6 mt-1 text-controls-highlight-palest absolute hidden peer-checked:block' />
+        <i
+          className='fi fi-br-check w-6 h-6 text-controls-highlight-palest
+        absolute top-3 left-3.5 transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100'
+        />
       </div>
       {rightLabel && (
         <label htmlFor={id} className={`text-sm font-light ps-2  ${disabled ? 'text-controls-content-disabled' : 'text-neutral-body'}`}>
@@ -41,6 +46,7 @@ export function Checkbox(props) {
 
 Checkbox.propTypes = {
   id: PropTypes.string.isRequired,
+  checked: PropTypes.bool.isRequired,
   disabled: PropTypes.bool.isRequired,
   leftLabel: PropTypes.string,
   rightLabel: PropTypes.string
