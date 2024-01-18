aef9a76138b0279563caffab8a661afe76404b16
Paul Graves <paul.graves@eca-international.com>
2024-01-18 14:21:33 +0000

    Feat: Add radio buttons

--- src/Radio/Radio.jsx
+++ src/Radio/Radio.jsx
@@ -2,30 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export function Radio(props) {
-  const { label, id, value, name, ...others } = props;
+  const { label, id, value, checked, name, ...others } = props;
 
   return (
-    <>
+    <div className='flex items-center text-neutral-body has-[:disabled]:text-controls-content-disabled'>
       <input
         type='radio'
         id={id}
         value={value}
         name={name}
+        checked={checked}
         {...others}
-        className='peer appearance-none w-6 h-6 border border-controls-border rounded-full transition-opacity
+        className='peer appearance-none w-6 h-6 border border-controls-border rounded-full place-content-center grid
 
-          hover:border-controls-border-hover hover:ring-2 hover:ring-offset-2 hover:ring-neutral-detail-paler
-          focus-visible:border-controls-border focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-controls-border
-          disabled:border-neutral-detail-paler disabled:hover:ring-0 disabled:cursor-not-allowed
+          disabled:border-neutral-detail-paler disabled:hover:outline-0 disabled:cursor-not-allowed
+          hover:border-controls-border-hover hover:outline hover:outline-2 hover:outline-offset-2 hover:outline-neutral-detail-paler hover:outline-offset-transparent
+          focus-visible:border-controls-border focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-controls-border
+          active:border-controls-border-hover active:[&:not(:disabled)]:bg-neutral-detail-palest active:outline active:outline-4 active:outline-offset-0 active:outline-neutral-detail-palest
 
-          active:border-controls-border-hover active:bg-neutral-detail-palest active:ring active:ring-offset-1 active:ring-offset-neutral-detail-palest active:ring-neutral-detail-palest'
+          before:content[""] before:w-3.5 before:h-3.5 before:bg-controls-highlight before:rounded-full before:transition before:scale-0
+
+          checked:border-controls-highlight
+          checked:before:scale-100
+          checked:disabled:border-controls-bg-disabled before:checked:disabled:bg-controls-bg-disabled
+          checked:hover:outline-controls-highlight-paler checked:hover:border-controls-highlight
+          checked:focus-visible:outline-controls-highlight checked:focus-visible:border-controls-highlight
+          checked:active:outline-neutral-detail-palest'
       />
       {label && (
-        <label htmlFor={id} className='text-sm font-light pe-2'>
+        <label htmlFor={id} className='text-sm font-light ps-2'>
           {label}
         </label>
       )}
-    </>
+    </div>
   );
 }
 
@@ -33,5 +42,6 @@ Radio.propTypes = {
   id: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  checked: PropTypes.bool,
   label: PropTypes.string
 };
--- src/Radio/Radio.stories.jsx
+++ src/Radio/Radio.stories.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Radio } from './Radio';
 
 export default {
@@ -13,17 +14,117 @@ export default {
     },
     id: {
       table: { disable: true }
+    },
+    value: {
+      table: { disable: true }
+    },
+    name: {
+      table: { disable: true }
+    },
+    checked: {
+      table: { disable: true }
+    },
+    label: {
+      table: { disable: true }
+    },
+    onChange: {
+      action: true,
+      table: { disable: true }
     }
   },
   args: {
-    disabled: false
+    disabled: false,
+    value: 'test'
   }
 };
 
+export const Default = {
+  args: {
+    name: 'default',
+    id: 'default'
+  },
+  render: (args) => (
+    <div className='flex flex-col justify-start'>
+      <div className='py-2'>
+        <Radio {...args} label='Radio 1' value='one' />
+      </div>
+      <div className='py-2'>
+        <Radio {...args} label='Radio 2' value='two' />
+      </div>
+    </div>
+  )
+};
+
 export const Selected = {
-  args: {}
+  args: {
+    checked: true,
+    id: 'selected',
+    name: 'selected'
+  }
 };
 
 export const Unselected = {
-  args: {}
+  args: {
+    checked: false,
+    id: 'unselected',
+    name: 'unselected'
+  }
+};
+
+export const Label = {
+  args: {
+    label: 'A Label',
+    id: 'label',
+    name: 'label'
+  }
+};
+
+export const Vertical = {
+  args: {
+    name: 'vertical'
+  },
+  render: (args) => (
+    <div className='flex flex-col justify-start'>
+      <div className='py-2'>
+        <Radio {...args} label='Apples' value='apples' />
+      </div>
+      <div className='py-2'>
+        <Radio {...args} label='Bananas' value='bananas' />
+      </div>
+      <div className='py-2'>
+        <Radio {...args} label='Oranges' value='oranges' />
+      </div>
+      <div className='py-2'>
+        <Radio {...args} label='Mangos' value='mangos' />
+      </div>
+      <div className='py-2'>
+        <Radio {...args} label='Strawberries' value='strawberries' />
+      </div>
+    </div>
+  )
+};
+
+export const Horizontal = {
+  args: {
+    name: 'horizontal'
+  },
+  render: (args) => (
+    <div className='flex flex-row items-center'>
+      <span className='pe-4'>
+        <Radio {...args} label='Apples' value='apples' />
+      </span>
+      <span className='pe-4'>
+        <Radio {...args} label='Bananas' value='bananas' />
+      </span>
+      <span className='pe-4'>
+        <Radio {...args} label='Oranges' value='oranges' />
+      </span>
+      <span className='pe-4'>
+        <Radio {...args} label='Mangos' value='mangos' />
+      </span>
+      <span className='pe-4'>
+        <Radio {...args} label='Strawberries' value='strawberries' />
+      </span>
+    </div>
+  )
 };
