2ece328e1f016b996047997ca24306152914c607
Glenn <glenn.knight@eca-international.com>
2024-02-21 07:00:43 +0000

    Feat: Added stories

--- src/Tooltip/Tooltip.stories.jsx
+++ src/Tooltip/Tooltip.stories.jsx
@@ -43,14 +43,8 @@ export default {
     state: 'info',
     content: 'Refresh',
     size: 'small',
-    position: 'top',
+    position: 'bottom',
     delay: 300
-  }
-};
-
-export const Default = {
-  args: {
-    name: 'default-tooltip'
   },
   render: (args) => (
     <Tooltip {...args}>
@@ -59,4 +53,87 @@ export const Default = {
       </Button>
     </Tooltip>
   )
+};
+
+export const Default = {
+  args: {
+    name: 'default-tooltip'
+  }
+};
+
+export const Info = {
+  args: {
+    name: 'info-tooltip',
+    state: 'info'
+  }
+};
+
+export const Warning = {
+  args: {
+    name: 'warning-tooltip',
+    state: 'warning'
+  }
+};
+
+export const Error = {
+  args: {
+    name: 'error-tooltip',
+    state: 'error'
+  }
+};
+
+export const Small = {
+  args: {
+    name: 'small-tooltip',
+    size: 'small'
+  }
+};
+
+export const Large = {
+  args: {
+    name: 'large-tooltip',
+    size: 'large'
+  }
+};
+
+export const Top = {
+  args: {
+    name: 'top-tooltip',
+    position: 'top'
+  }
+};
+
+export const Right = {
+  args: {
+    name: 'right-tooltip',
+    position: 'right'
+  }
+};
+
+export const Bottom = {
+  args: {
+    name: 'bottom-tooltip',
+    position: 'bottom'
+  }
+};
+
+export const Left = {
+  args: {
+    name: 'left-tooltip',
+    position: 'left'
+  }
+};
+
+export const NoDelay = {
+  args: {
+    name: 'left-tooltip',
+    delay: 0
+  }
+};
+
+export const Delay = {
+  args: {
+    name: 'left-tooltip',
+    delay: 500
+  }
 };
