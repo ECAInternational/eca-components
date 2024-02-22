728c7d1917f2ea86333b51a8842027859db9f060
Glenn <glenn.knight@eca-international.com>
2024-02-22 07:38:28 +0000

    Feat: Simplify styles

--- src/SegmentedButtons/SegmentedButtons.jsx
+++ src/SegmentedButtons/SegmentedButtons.jsx
@@ -7,26 +7,22 @@ export function SegmentedButtons(props) {
   return (
     <div className='flex w-full'>
       {React.Children.map(children, (child) => (
-        <label
-          key={child.props['aria-label']}
-          htmlFor={child.props.id}
-          className='relative group flex-1 cursor-pointer 
-                text-sm text-neutral-body
-                bg-controls-bg-unselected has-[:checked]:bg-neutral-layer-2 has-[:focus-visible]:z-0 z-10
-                border border-controls-border border-l-0 first:border-l first:rounded-l last:rounded-r
-                hover:border-controls-border-hover hover:border-l
-                [&:has(+:hover)]:border-r-0'
-        >
+        <>
           {React.cloneElement(child, {
-            className: `after:content-[attr(aria-label)] cursor-pointer appearance-none peer py-3 w-full text-center 
-              group-only:rounded group-first:rounded-l group-last:rounded-r transition
-              outline-2 outline-offset-2 outline-default-transparent focus:outline-controls-border focus:outline-offset-4 focus:checked:outline-controls-highlight`
+            className: `cursor-pointer peer appearance-none w-full z-10 pt-3 relative overflow-hidden
+            text-sm text-neutral-body gap-1.5
+            flex flex-col items-center justify-center
+            bg-controls-bg-unselected checked:bg-neutral-layer-2 focus:z-0 
+            border border-controls-border border-l-0 first:border-l first:rounded-l last:rounded-r
+            hover:border-controls-border-hover
+            before:content-[attr(aria-label)]
+            after:content-[''] after:h-1.5 after:w-full
+            after:checked:bg-controls-highlight 
+            after:hover:checked:bg-controls-highlight 
+            hover:after:bg-neutral-detail-paler
+            outline outline-2 outline-offset-2 outline-default-transparent focus:outline-controls-border focus:outline-offset-4 focus:checked:outline-controls-highlight`
           })}
-          <div
-            className='h-1.5 w-full peer-checked:bg-controls-highlight absolute bottom-0 
-          group-hover:bg-neutral-detail-paler group-only:rounded-b group-first:rounded-bl group-last:rounded-br'
-          />
-        </label>
+        </>
       ))}
     </div>
   );
