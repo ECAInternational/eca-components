f49dc16abb243baf9894fb8bc5b697a65ca9e2db
Glenn <glenn.knight@eca-international.com>
2024-02-27 08:19:24 +0000

    Feat: Added label

--- src/SegmentedButtons/SegmentedButtons.jsx
+++ src/SegmentedButtons/SegmentedButtons.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export function SegmentedButtons(props) {
-  const { children, disabled, size = 'medium' } = props;
+  const { id, name, children, disabled, size = 'medium', label, description } = props;
 
   const sizes = {
     input: {
@@ -15,13 +15,23 @@ export function SegmentedButtons(props) {
     }
   };
 
+  const labelID = `${id || name}-label`;
+
   return (
-    <div className='flex w-full'>
-      {React.Children.map(children, (child) => (
-        <>
-          {React.cloneElement(child, {
-            disabled: disabled || child.props.disabled,
-            className: `cursor-pointer peer appearance-none w-full z-10 relative overflow-hidden transition text-neutral-body
+    <div role='radiogroup' aria-labelledby={labelID} className='text-controls-content-disabled has-[:enabled]:text-neutral-detail-bolder'>
+      {label && (
+        <label id={labelID} className='text-sm block py-1 transition-all'>
+          {label}
+          {description && <span className='font-light ps-1'>{description}</span>}
+        </label>
+      )}
+      <div className='flex'>
+        {React.Children.map(children, (child) => (
+          <>
+            {React.cloneElement(child, {
+              disabled: disabled || child.props.disabled,
+              name,
+              className: `cursor-pointer peer appearance-none w-full z-10 relative overflow-hidden transition text-neutral-body
             flex flex-col items-center justify-center bg-controls-bg-unselected ${sizes.input[size]}
             border border-controls-border border-l-0 first:border-l first:rounded-l last:rounded-r
             outline outline-2 outline-offset-2 outline-default-transparent
@@ -29,18 +39,21 @@ export function SegmentedButtons(props) {
             after:content-[''] ${sizes.highlight[size]} after:w-full after:checked:bg-controls-highlight after:hover:checked:bg-controls-highlight
             checked:bg-neutral-layer-2
             hover:after:bg-neutral-detail-paler hover:border-controls-border-hover
-            focus:z-0 focus:outline-controls-border focus:outline-offset-4 focus:checked:outline-controls-highlight
+            focus-visible:z-0 focus-visible:outline-controls-border focus-visible:outline-offset-4 focus-visible:checked:outline-controls-highlight
             active:bg-controls-highlight-palest active:after:bg-controls-highlight-pale active:checked:bg-neutral-layer-2
             disabled:bg-neutral-layer-1 disabled:text-controls-content-disabled disabled:border-neutral-detail-paler disabled:cursor-not-allowed
             disabled:after:bg-default-transparent disabled:checked:after:bg-controls-highlight-pale`
-          })}
-        </>
-      ))}
+            })}
+          </>
+        ))}
+      </div>
     </div>
   );
 }
 
 SegmentedButtons.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   disabled: PropTypes.bool,
   size: PropTypes.oneOf(['small', 'medium'])
