6f493c46f133421b2cfe2256e3b11e0cbf3d82da
Paul Graves <paul.graves@eca-international.com>
2024-03-14 17:09:15 +0000

    Fix: Make it work

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,38 @@ const ecaBrand = require('@ecainternational/eca-design-tokens/tailwind/eca-brand
 const ecaDark = require('@ecainternational/eca-design-tokens/tailwind/eca-dark-theme.tailwind');
 const ecaLight = require('@ecainternational/eca-design-tokens/tailwind/eca-light-theme.tailwind');
 
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  theme: {
-    ...ecaBrand,
-    extend: {
-      transitionDuration: {
-        DEFAULT: '200ms'
-      },
-      transitionTimingFunction: {
-        DEFAULT: 'ease-in-out'
-      },
-      scale: {
-        92: '0.92'
-      }
+const theme = {
+  ...ecaBrand,
+  extend: {
+    transitionDuration: {
+      DEFAULT: '200ms'
     },
-    boxShadow: {
-      'inner-button': 'inset 0 0 0 0.2rem rgba(0, 0, 0, 1)'
+    transitionTimingFunction: {
+      DEFAULT: 'ease-in-out'
+    },
+    scale: {
+      92: '0.92'
     }
   },
+  boxShadow: {
+    'inner-button': 'inset 0 0 0 0.2rem rgba(0, 0, 0, 1)'
+  }
+};
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme,
   plugins: [
     createThemes({
       'eca-light': ecaLight,
       'eca-dark': ecaDark
+    }),
+    plugin(({ addBase }) => {
+      addBase(theme);
+      createThemes({
+        'eca-light': ecaLight,
+        'eca-dark': ecaDark
+      });
     })
   ]
 };
