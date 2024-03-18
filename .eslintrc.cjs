faf39c52eeb6577c9156fc78a6c1d07bfca8562d
rexebin <rexebin@gmail.com>
2024-03-18 17:55:00 +0000

    Feat: add card components Chore: load font in storybook Chore: add view ports to storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['airbnb', 'prettier', 'plugin:storybook/recommended'],
+  extends: ['airbnb', 'prettier', 'plugin:storybook/recommended', 'plugin:tailwindcss/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   overrides: [
