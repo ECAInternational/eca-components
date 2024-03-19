07ffb2de3c658ddee1521c1aa36fac4f47129ff8
rexebin <rexebin@gmail.com>
2024-03-19 14:26:51 +0000

    update exports

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,9 @@ import { TextInput } from './TextInput/TextInput.tsx';
 import { Chip } from './Chip/Chip.tsx';
 import { Tooltip } from './Tooltip/Tooltip.tsx';
 
-export * from './Card/index.ts';
+import { Card, CardBody, CardFooter, CardHeader } from './Card/index.ts';
+
+export { Card, CardBody, CardFooter, CardHeader };
 export { Button };
 export { Checkbox };
 export { IconButton };
