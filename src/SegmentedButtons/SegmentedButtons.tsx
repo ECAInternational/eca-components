8a868ffc9465674b18c7e0c677f8b15a818bc8c6
Glenn <glenn.knight@eca-international.com>
2024-03-21 06:47:11 +0000

    Fix: Update focus state

--- src/SegmentedButtons/SegmentedButtons.tsx
+++ src/SegmentedButtons/SegmentedButtons.tsx
@@ -29,29 +29,27 @@ export function SegmentedButtons(props: SegmentedButtonsProps) {
   return (
     <div role='radiogroup' aria-labelledby={labelID} className='text-controls-content-disabled has-[:enabled]:text-neutral-detail-bolder'>
       {label && (
-        <span id={labelID} className='text-sm block py-1 transition-all'>
+        <span id={labelID} className='block py-1 text-sm transition-all'>
           {label}
-          {description && <span className='font-light ps-1'>{description}</span>}
+          {description && <span className='ps-1 font-light'>{description}</span>}
         </span>
       )}
-      <div className='flex'>
+      <div className='flex rounded outline outline-2 outline-offset-2 outline-default-transparent focus-within:outline-offset-4 focus-within:outline-controls-highlight'>
         {Children.map(children, (child) => (
           <>
             {cloneElement(child, {
               disabled: disabled || child.props.disabled,
               name,
               className: `cursor-pointer peer appearance-none w-full z-10 relative overflow-hidden transition text-neutral-body
-            flex flex-col items-center justify-center bg-controls-bg-unselected ${sizes.input[size]}
-            border border-controls-border border-l-0 first:border-l first:rounded-l last:rounded-r
-            outline outline-2 outline-offset-2 outline-default-transparent
-            before:content-[attr(aria-label)] before:text-sm
-            after:content-[''] ${sizes.highlight[size]} after:w-full after:checked:bg-controls-highlight after:hover:checked:bg-controls-highlight
-            checked:bg-neutral-layer-2
-            hover:after:bg-neutral-detail-paler hover:border-controls-border-hover
-            focus-visible:z-0 focus-visible:outline-controls-border focus-visible:outline-offset-4 focus-visible:checked:outline-controls-highlight
-            active:bg-controls-highlight-palest active:after:bg-controls-highlight-pale active:checked:bg-neutral-layer-2
-            disabled:bg-neutral-layer-1 disabled:text-controls-content-disabled disabled:border-neutral-detail-paler disabled:cursor-not-allowed
-            disabled:after:bg-default-transparent disabled:checked:after:bg-controls-highlight-pale`
+              flex flex-col items-center justify-center bg-controls-bg-unselected ${sizes.input[size]}
+              border border-controls-border border-l-0 outline-none first:border-l first:rounded-l last:rounded-r
+              before:content-[attr(aria-label)] before:text-sm
+              after:content-[''] ${sizes.highlight[size]} after:w-full after:checked:bg-controls-highlight after:hover:checked:bg-controls-highlight
+              checked:bg-neutral-layer-2
+              hover:after:bg-neutral-detail-paler hover:border-controls-border-hover
+              active:bg-controls-highlight-palest active:after:bg-controls-highlight-pale active:checked:bg-neutral-layer-2
+              disabled:bg-neutral-layer-1 disabled:text-controls-content-disabled disabled:border-neutral-detail-paler disabled:cursor-not-allowed
+              disabled:after:bg-default-transparent disabled:checked:after:bg-controls-highlight-pale`
             })}
           </>
         ))}
