888db60f605ffd67c49ffedcb48a251144e7542f
Paul Graves <paul.graves@eca-international.com>
2024-03-26 08:29:34 +0000

    Fix: Simplify code

--- src/tailwindPlugin.ts
+++ src/tailwindPlugin.ts
@@ -2,26 +2,23 @@ import plugin from 'tailwindcss/plugin';
 import ecaTheme from './eca-theme.ts';
 
 export const tailwindPlugin = plugin(({ addUtilities, theme }) => {
-  const typographyStyles = {
+  const typographyStyles: any = {
     display: theme('display'),
     heading: theme('heading'),
     label: theme('label'),
     paragraph: theme('paragraph')
   };
 
-  const newUtilities: Record<string, any> = Object.entries(typographyStyles).reduce(
-    (acc, [parentKey, parentValue]) => ({
-      ...acc,
-      ...Object.entries(parentValue as Record<string, any>).reduce(
-        (acc2, [childKey, childValue]) => ({
-          ...acc2,
-          [`.${parentKey}-${childKey}`]: childValue
-        }),
-        {}
-      )
-    }),
-    {}
-  );
+  const newUtilities: Record<string, any> = {};
+
+  Object.keys(typographyStyles).forEach((parentKey) => {
+    const parentValue = typographyStyles[parentKey];
+    const childKeys = Object.keys(parentValue);
+
+    childKeys.forEach((childKey) => {
+      newUtilities[`.${parentKey}-${childKey}`] = parentValue[childKey];
+    });
+  });
 
   addUtilities(newUtilities);
 }, ecaTheme);
