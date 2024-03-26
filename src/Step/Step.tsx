c8c4a8e93641ddb1781549bcd92131553d682171
Glenn <glenn.knight@eca-international.com>
2024-03-26 06:55:57 +0000

    Feat: Updated step styles

--- src/Step/Step.tsx
+++ src/Step/Step.tsx
@@ -11,10 +11,16 @@ export interface StepProps {
 export function Step(props: StepProps) {
   const { index, label, state = 'default' } = props;
 
+  const text: Record<StepState, string> = {
+    default: 'text-neutral-detail-bold hover:text-neutral-detail-bolder focus-visible:text-neutral-detail-boldest active:text-neutral-detail-boldest',
+    completed: 'text-neutral-detail-bold',
+    error: 'text-states-error-bold active:text-states-error-boldest'
+  };
+
   const disc: Record<StepState, string> = {
-    default: 'border border-controls-border text-neutral/detail-bold',
-    completed: 'bg-controls-highlight-paler text-controls-highlight-bolder',
-    error: 'border border-states-error text-states-error'
+    default: 'border border-controls-border text-neutral-detail-bold group-hover:outline-neutral-detail-palest group-focus-visible:outline-controls-border group-active:outline-neutral-detail-paler',
+    completed: 'bg-controls-highlight-paler text-controls-highlight-bolder group-hover:outline-controls-highlight-palest group-focus-visible:outline-controls-highlight group-active:outline-controls-highlight-paler',
+    error: 'border border-states-error text-states-error group-hover:outline-states-error-paler group-focus-visible:outline-pink-500 group-active:outline-states-error-paler group-active:border-none group-active:bg-states-error-paler'
   };
 
   const discContent: Record<StepState, ReactElement> = {
@@ -24,14 +30,14 @@ export function Step(props: StepProps) {
   };
 
   return (
-    <span className='flex items-center text-sm '>
+    <button className={`group flex items-center cursor-pointer outline-0 text-sm ${text[state]}`}>
       {label && <span className='pr-4'>{label}</span>}
-      <button
-        className={`size-5.5 m-1 rounded-full flex items-center justify-center ${disc[state]}
-        outline-offset hover:outline`}
+      <span
+        className={`size-5.5 m-1 rounded-full flex items-center justify-center ${disc[state]} font-medium
+        outline outline-2 outline-offset-2 outline-default-transparent`}
       >
         {discContent[state]}
-      </button>
-    </span>
+      </span>
+    </button>
   );
 }
