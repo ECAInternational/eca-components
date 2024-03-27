0b42d3da0a3cbd88c974364eaacf4eac0928ae9d
rexebin <rexebin@gmail.com>
2024-03-27 16:16:28 +0000

    chore: add bg color to options.

--- src/AutoComplete/AutoComplete.Options.tsx
+++ src/AutoComplete/AutoComplete.Options.tsx
@@ -9,7 +9,7 @@ export function AutoCompleteOptions({
 }) {
   return (
     <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0' afterLeave={() => onClose && onClose()}>
-      <Combobox.Options className='absolute mt-2 max-h-60 w-full overflow-auto rounded border border-neutral-detail-paler shadow-lg paragraph-sm-lighter focus:outline-none' {...props} />
+      <Combobox.Options className='absolute mt-2 max-h-60 w-full overflow-auto rounded border border-neutral-detail-paler bg-neutral-layer-2 shadow-lg paragraph-sm-lighter focus:outline-none' {...props} />
     </Transition>
   );
 }
--- src/AutoComplete/Label.tsx
+++ src/AutoComplete/Label.tsx
@@ -2,7 +2,7 @@ import React, { LabelHTMLAttributes, PropsWithChildren } from 'react';
 
 export function Label({ className, children, htmlFor, ...props }: PropsWithChildren<LabelHTMLAttributes<HTMLLabelElement>>) {
   return (
-    <label className={`block py-1 transition-all label-sm-mid ${className}`} htmlFor={htmlFor} {...props}>
+    <label className={`block py-1 text-neutral-body transition-all label-sm-mid ${className}`} htmlFor={htmlFor} {...props}>
       {children}
     </label>
   );
