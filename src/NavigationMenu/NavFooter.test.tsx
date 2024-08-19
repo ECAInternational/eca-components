3054922224c83f4fdca015de5e4e62837d912b55
Paul Graves <paul.graves@eca-international.com>
2024-08-19 17:26:27 +0100

    Getting tests to run

--- src/NavigationMenu/NavFooter.test.tsx
+++ src/NavigationMenu/NavFooter.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { NavFooter } from './nav-footer';
+import { NavFooter } from './NavFooter.tsx';
 
 describe('NavFooter', () => {
   it('should display CollapseIcon and title "Collapse" when isNavExpanded is true', () => {
