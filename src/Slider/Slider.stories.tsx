6cbe129ca3438320ab4a35baf173b848393c5c3e
Glenn <glenn.knight@eca-international.com>
2024-08-20 10:10:32 +0100

    Added stories

--- src/Slider/Slider.stories.tsx
+++ src/Slider/Slider.stories.tsx
@@ -1,4 +1,5 @@
-import { Slider } from './Slider.tsx';
+import React, { useState } from 'react';
+import { Slider, SliderProps } from './Slider.tsx';
 
 export default {
   component: Slider,
@@ -10,18 +11,78 @@ export default {
     id: {
       table: { disable: true }
     },
+    name: {
+      table: { disable: true }
+    },
     type: {
       table: { disable: true }
     },
     className: {
       table: { disable: true }
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Disables the slider'
+    },
+    min: {
+      control: 'number',
+      description: 'Minimum value of the slider'
+    },
+    max: {
+      control: 'number',
+      description: 'Maximum value of the slider'
+    },
+    marks: {
+      control: 'array',
+      description: 'Marks for the slider'
+    },
+    variant: {
+      control: 'select',
+      options: ['discrete'],
+      description: 'Variant of the slider',
+      defaultValue: { summary: 'discrete' }
+    },
+    value: {
+      control: 'number',
+      description: 'Value of the slider'
     }
   },
-  args: {}
+  args: {
+    disabled: false,
+    min: 0,
+    max: 5,
+    value: 2,
+    marks: [
+      { value: 0, label: 'Value 0' },
+      { value: 1, label: 'Value 1' },
+      { value: 2, label: 'Value 2' },
+      { value: 3, label: 'Value 3' },
+      { value: 4, label: 'Value 4' },
+      { value: 5, label: 'Value 5' }
+    ]
+  },
+  render: (args: SliderProps) => {
+    const [value, setValue] = useState<number>(args.value);
+    return <Slider {...args} value={value} onChange={setValue} />;
+  }
 };
 
 export const Default = {
   args: {
-    name: 'default-button'
+    name: 'default-slider'
+  }
+};
+
+export const Disabled = {
+  args: {
+    name: 'disabled-slider',
+    disabled: true
+  }
+};
+
+export const Discrete = {
+  args: {
+    name: 'discrete-slider',
+    variant: 'discrete'
   }
 };
