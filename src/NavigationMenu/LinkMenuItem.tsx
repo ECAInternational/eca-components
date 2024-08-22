b607ee6bf51cf305ba825f2dbd6443c3e7762686
Natalie Akam <natalie.akam@eca-international.com>
2024-08-22 16:58:03 +0100

    feat: tighten up url matching

--- src/NavigationMenu/LinkMenuItem.tsx
+++ src/NavigationMenu/LinkMenuItem.tsx
@@ -11,7 +11,7 @@ export function LinkMenuItem({ url, details, isNavExpanded, isTopLevel, isConten
     <>
       <div className='menuItem relative'>
         {!isTopLevel && <div data-testid='menu-branch' className='branch' />}
-        <a href={enabled ? link : ''} className={`${enabled ? 'text-neutral-body' : 'pointer-events-none text-controls-content-disabled'} ${isSelected && 'bg-controls-element-tonal'} link selected:bg-controls-element-tonal flex min-h-5 w-full items-center rounded-md px-3 py-2.5 hover:bg-controls-element-tonal-hover focus-visible:border-controls-highlight`} aria-label={label} onClick={onNavigate}>
+        <a href={enabled ? link : ''} className={`${enabled ? 'text-neutral-body' : 'pointer-events-none text-controls-content-disabled'} ${isSelected && 'bg-controls-element-tonal'} link selected:bg-controls-element-tonal hover:bg-controls-element-tonal-hover flex min-h-5 w-full items-center rounded-md px-3 py-2.5 focus-visible:border-controls-highlight`} aria-label={label} onClick={onNavigate}>
           {isTopLevel ? <MenuItemTitle details={details} isNavExpanded={isNavExpanded} /> : label}
         </a>
       </div>
