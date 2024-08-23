3f5625868eab87290b26ef7f43d899e1526299c2
Natalie Akam <natalie.akam@eca-international.com>
2024-08-23 12:18:06 +0100

    Let prettier render code unreadable

--- src/NavigationMenu/LinkMenuItem.tsx
+++ src/NavigationMenu/LinkMenuItem.tsx
@@ -3,21 +3,7 @@ import type { MenuItemDetails } from './types';
 import { MenuItemTitle } from './MenuItemTitle.tsx';
 import { PopOutContent } from './PopOutContent.tsx';
 
-export function LinkMenuItem({
-  url,
-  details,
-  isNavExpanded,
-  isTopLevel,
-  isContentVisible,
-  onNavigate,
-}: {
-  url: string;
-  details: MenuItemDetails;
-  isNavExpanded: boolean;
-  isTopLevel?: boolean;
-  isContentVisible?: boolean;
-  onNavigate?: () => void;
-}) {
+export function LinkMenuItem({ url, details, isNavExpanded, isTopLevel, isContentVisible, onNavigate }: { url: string; details: MenuItemDetails; isNavExpanded: boolean; isTopLevel?: boolean; isContentVisible?: boolean; onNavigate?: () => void }) {
   const { link = '', label, enabled } = details;
 
   const linkPathname = new URL(link, 'http://placeholder.com').pathname;
@@ -29,24 +15,11 @@ export function LinkMenuItem({
     <>
       <div className='menuItem relative'>
         {!isTopLevel && <div data-testid='menu-branch' className='branch' />}
-        <a
-          href={enabled ? link : ''}
-          className={`${enabled ? 'text-neutral-body' : 'pointer-events-none text-controls-content-disabled'} ${isSelected && 'bg-controls-element-tonal'} link selected:bg-controls-element-tonal flex min-h-5 w-full items-center rounded-md px-3 py-2.5 hover:bg-controls-element-tonal-hover focus-visible:border-controls-highlight`}
-          aria-label={label}
-          onClick={onNavigate}
-        >
+        <a href={enabled ? link : ''} className={`${enabled ? 'text-neutral-body' : 'pointer-events-none text-controls-content-disabled'} ${isSelected && 'bg-controls-element-tonal'} link selected:bg-controls-element-tonal flex min-h-5 w-full items-center rounded-md px-3 py-2.5 hover:bg-controls-element-tonal-hover focus-visible:border-controls-highlight`} aria-label={label} onClick={onNavigate}>
           {isTopLevel ? <MenuItemTitle details={details} isNavExpanded={isNavExpanded} /> : label}
         </a>
       </div>
-      {isTopLevel && (
-        <PopOutContent
-          url={url}
-          menuItemDetails={details}
-          isContentVisible={isTopLevel && isContentVisible}
-          shouldShowSubItems
-          isNavExpanded={isNavExpanded}
-        />
-      )}
+      {isTopLevel && <PopOutContent url={url} menuItemDetails={details} isContentVisible={isTopLevel && isContentVisible} shouldShowSubItems isNavExpanded={isNavExpanded} />}
     </>
   );
 }
--- src/NavigationMenu/TopLevelMenuItem.tsx
+++ src/NavigationMenu/TopLevelMenuItem.tsx
@@ -3,19 +3,7 @@ import type { MenuItemDetails } from './types';
 import { LinkMenuItem } from './LinkMenuItem.tsx';
 import { ExpandableMenuItem } from './ExpandableMenuItem.tsx';
 
-export function TopLevelMenuItem({
-  url,
-  menuItemDetails,
-  isListExpanded,
-  collapseSubItemsExcept,
-  isNavExpanded,
-}: {
-  url: string;
-  menuItemDetails: MenuItemDetails;
-  isListExpanded: boolean;
-  collapseSubItemsExcept: (expandedMenuItemId: string) => void;
-  isNavExpanded: boolean;
-}) {
+export function TopLevelMenuItem({ url, menuItemDetails, isListExpanded, collapseSubItemsExcept, isNavExpanded }: { url: string; menuItemDetails: MenuItemDetails; isListExpanded: boolean; collapseSubItemsExcept: (expandedMenuItemId: string) => void; isNavExpanded: boolean }) {
   const [isHovering, setIsHovering] = useState(false);
   const { id, subItems } = menuItemDetails;
 
@@ -36,33 +24,8 @@ export function TopLevelMenuItem({
   const isContentVisible = isListExpanded || (!isNavExpanded && isHovering);
 
   return (
-    <li
-      className='flex flex-col justify-between'
-      onFocus={handleMouseOver}
-      onMouseOver={handleMouseOver}
-      onBlur={handleMouseOut}
-      onMouseOut={handleMouseOut}
-    >
-      {subItems ? (
-        <ExpandableMenuItem
-          url={url}
-          details={menuItemDetails}
-          isNavExpanded={isNavExpanded}
-          onClick={onToggleMenuItem}
-          isListExpanded={isListExpanded}
-          isContentVisible={isContentVisible}
-          onNavigate={onNavigate}
-        />
-      ) : (
-        <LinkMenuItem
-          url={url}
-          onNavigate={onNavigate}
-          details={menuItemDetails}
-          isNavExpanded={isNavExpanded}
-          isTopLevel
-          isContentVisible={isContentVisible}
-        />
-      )}
+    <li className='flex flex-col justify-between' onFocus={handleMouseOver} onMouseOver={handleMouseOver} onBlur={handleMouseOut} onMouseOut={handleMouseOut}>
+      {subItems ? <ExpandableMenuItem url={url} details={menuItemDetails} isNavExpanded={isNavExpanded} onClick={onToggleMenuItem} isListExpanded={isListExpanded} isContentVisible={isContentVisible} onNavigate={onNavigate} /> : <LinkMenuItem url={url} onNavigate={onNavigate} details={menuItemDetails} isNavExpanded={isNavExpanded} isTopLevel isContentVisible={isContentVisible} />}
     </li>
   );
 }
