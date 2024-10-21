d328b874a202a68a88b8b0a2474290328e1840dc
Natalie Akam <natalie.akam@eca-international.com>
2024-10-21 10:12:35 +0100

    Fix nav styling

--- src/NavigationMenu/LinkMenuItem.tsx
+++ src/NavigationMenu/LinkMenuItem.tsx
@@ -3,6 +3,7 @@ import type { MenuItemDetails } from './types';
 import { MenuItemTitle } from './MenuItemTitle.tsx';
 import { PopOutContent } from './PopOutContent.tsx';
 import isLinkCurrentLocation from './isLinkCurrentLocation.ts';
+import styles from './LinkMenuItem.module.css';
 
 export function LinkMenuItem({ url, details, isNavExpanded, isTopLevel, isContentVisible, onNavigate }: { url: string; details: MenuItemDetails; isNavExpanded: boolean; isTopLevel?: boolean; isContentVisible?: boolean; onNavigate?: () => void }) {
   const { link = '', label, enabled } = details;
@@ -11,8 +12,8 @@ export function LinkMenuItem({ url, details, isNavExpanded, isTopLevel, isConten
 
   return (
     <>
-      <div className='menuItem'>
-        {!isTopLevel && <div data-testid='menu-branch' className='branch' />}
+      <div className={`relative ${styles.menuItem}`}>
+        {!isTopLevel && <div data-testid='menu-branch' className={styles.branch} />}
         <a href={enabled ? link : ''} className={`${enabled ? 'text-neutral-body' : 'pointer-events-none text-controls-content-disabled'} ${isSelected && 'bg-controls-element-tonal'} link selected:bg-controls-element-tonal flex min-h-5 w-full items-center rounded-md px-3 py-2.5 hover:bg-controls-element-tonal-hover focus-visible:border-controls-highlight`} aria-label={label} onClick={onNavigate}>
           {isTopLevel ? <MenuItemTitle details={details} isNavExpanded={isNavExpanded} /> : label}
         </a>
