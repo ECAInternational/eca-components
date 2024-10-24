6974415828dbc8a87e6f32c05a9c7d19232d74a1
Glenn Knight <glenn.knight@eca-international.com>
2024-10-24 10:34:55 +0100

    Remove margins from ComboboxOptions and ListboxOptions

--- src/Select/Select.Options.tsx
+++ src/Select/Select.Options.tsx
@@ -10,7 +10,7 @@ export function SelectOptions({
 }) {
   return (
     <Transition as={Fragment} leave='transition ease-in duration-150' leaveFrom='opacity-100' leaveTo='opacity-0' afterLeave={() => onClose && onClose()}>
-      <ListboxOptions anchor={{ to: 'bottom', gap: '8px' }} className={`z-50 !my-0 !max-h-60 min-w-[var(--button-width)] overflow-auto rounded-md border border-neutral-detail-paler bg-neutral-layer-2 paragraph-sm-lighter focus:outline-none ${className}`} {...props} />
+      <ListboxOptions anchor={{ to: 'bottom', gap: '8px' }} className={`z-50 !max-h-60 min-w-[var(--button-width)] overflow-auto rounded-md border border-neutral-detail-paler bg-neutral-layer-2 paragraph-sm-lighter focus:outline-none ${className}`} {...props} />
     </Transition>
   );
 }
