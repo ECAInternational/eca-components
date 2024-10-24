14b8545c537d3f8de2535c30c1f7f861d59423bc
Glenn Knight <glenn.knight@eca-international.com>
2024-10-24 10:48:10 +0100

    Add align and position props to dropdowns

--- src/Select/Select.Options.tsx
+++ src/Select/Select.Options.tsx
@@ -4,13 +4,17 @@ import { ListboxOptions, Transition } from '@headlessui/react';
 export function SelectOptions({
   className,
   onClose,
+  position = 'bottom',
+  align = 'start',
   ...props
 }: ComponentPropsWithRef<typeof ListboxOptions> & {
   onClose?: () => void;
+  position: 'top' | 'bottom' | 'left' | 'right';
+  align: 'start' | 'end';
 }) {
   return (
     <Transition as={Fragment} leave='transition ease-in duration-150' leaveFrom='opacity-100' leaveTo='opacity-0' afterLeave={() => onClose && onClose()}>
-      <ListboxOptions anchor={{ to: 'bottom', gap: '8px' }} className={`z-50 !max-h-60 min-w-[var(--button-width)] overflow-auto rounded-md border border-neutral-detail-paler bg-neutral-layer-2 paragraph-sm-lighter focus:outline-none ${className}`} {...props} />
+      <ListboxOptions anchor={{ to: `${position} ${align}`, gap: '8px' }} className={`z-50 !max-h-60 min-w-[var(--button-width)] overflow-auto rounded-md border border-neutral-detail-paler bg-neutral-layer-2 paragraph-sm-lighter focus:outline-none ${className}`} {...props} />
     </Transition>
   );
 }
