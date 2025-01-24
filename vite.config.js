9076dd3c217ce4363803411c052d9ce7437e3dc5
Natalie Akam <natalie.akam@eca-international.com>
2025-01-24 15:44:49 +0000

    Fix vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   optimizeDeps: {
@@ -7,7 +9,7 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')]
+      plugins: [tailwindcss, autoprefixer]
     }
   },
   plugins: [
